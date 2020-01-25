0e41e6cf4fb8d2f9e38385503f6e88c41f1f8e85
Tyll'a <psduckie@yahoo.com>
2020-01-25 03:23:57 -0500

    Added the Power class

---
+++ Power.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace RPR
+{
+    class Power
+    {
+        private String powerName;
+        private int numSlots;
+
+        public String getPowerName()
+        {
+            return powerName;
+        }
+
+        public void setPowerName(String newName)
+        {
+            powerName = newName;
+        }
+
+        public int getNumSlots()
+        {
+            return numSlots;
+        }
+
+        public void setNumSlots(int newNum)
+        {
+            if(newNum > 6)
+            {
+                Debug.Print("Number of slots too high.");
+            } else if(newNum < 1)
+            {
+                Debug.Print("Number of slots too low.");
+            } else
+            {
+                numSlots = newNum;
+            }
+        }
+    }
+}
