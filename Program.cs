b650a06c6cfb5a70bcba058d50eec14ff0eb58f7
Tyll'a <psduckie@yahoo.com>
2020-02-09 00:07:11 -0500

    Added origin generation to program.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     using System;
     using System.IO;
     using System.Data;
+    using System.Collections.Generic;
     using Newtonsoft.Json;
     using MySql.Data;
     using MySql.Data.MySqlClient;
@@ -11,9 +12,10 @@
 
     class Program
     {
+        static Random rand = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Connect to MySQL database
             DBConnection conn = new DBConnection();
             string connString = File.ReadAllText(@"..\..\..\db\dbconfig.json");
             conn = JsonConvert.DeserializeObject<DBConnection>(connString);
@@ -28,8 +30,42 @@
             {
                 Console.WriteLine(e.ToString());
             }
+
+            // Create character variable
+            Character myChar = new Character();
+
+            // Roll origin
+            try
+            {
+                string cmdStr = "SELECT * FROM origins";
+                MySqlCommand cmd = new MySqlCommand(cmdStr, mySqlConn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                Queue<DBItem> results = new Queue<DBItem>();
+                while(rdr.Read())
+                {
+                    results.Enqueue(new DBItem(rdr[1].ToString(), (int)rdr[0]));
+                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+                int index = rand.Next(results.Count);
+                DBItem dump; // For debug use only
+
+                for(int i = 0; i < index; i++)
+                {
+                   dump = results.Dequeue();  // Dump this item 
+                   Console.WriteLine($"Dumping {dump.ItemName}");
+                }
+
+                myChar.Origin = results.Dequeue();
+                Console.WriteLine($"Keeping {myChar.Origin.ItemName}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            // Close connection
             mySqlConn.Close();
-            Console.WriteLine("This seems to work.");
         }
     }
 }
